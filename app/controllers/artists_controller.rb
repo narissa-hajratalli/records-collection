1bcdc70dde9a1ec755dfe71c9f299225236dfd9c
NARISSA <nkh239@nyu.edu>
2020-08-21 10:06:20 -0400

    Get artist by id

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,18 @@
 class ArtistsController < ApplicationController
+  # GET - show ONE artist
+  def show
+    @single_artist = Artist.exists?(params[:id])
+
+    if @single_artist
+      render :json => {
+          :response => "Successful.",
+          :data => Artist.find(params[:id])
+      }
+    else
+      render :json => {
+          :response => "Artist not found."
+      }
+    end
+  end
+
 end
