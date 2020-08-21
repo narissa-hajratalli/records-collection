03c800a24ffaf60988221676ec2a7109dd5883e4
Kristy Lee <leekristy01@gmail.com>
2020-08-21 09:57:02 -0400

    added the create artist route

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,18 @@
 class ArtistsController < ApplicationController
+  def create
+    @new_artist = Artist.new(artist_params)
+    if @new_artist.save
+      render :json => {
+          :data => @new_artist
+      }
+    else
+      render :json => {
+          :response => "unable to create new artist"
+      }
+    end
+  end
+
+  def artist_params
+    params.permit(:name, :hot_100_hits)
+  end
 end
